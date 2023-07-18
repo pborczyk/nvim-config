6a8ff752c9ed6c5864ae22b0dddc52a8869c968c
Piotr Borczyk <howorang@gmail.com>
2023-07-19 01:23:00 +0200

    Improved lsp config

--- lua/howo/lsp.lua
+++ lua/howo/lsp.lua
@@ -1,7 +1,11 @@
 local lsp = require('lsp-zero')
 
 lsp.preset("recommended")
-
+lsp.ensure_installed({
+'tsserver',
+'eslint',
+'lua_ls'
+})
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
